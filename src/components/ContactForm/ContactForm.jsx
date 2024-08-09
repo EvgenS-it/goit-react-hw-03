4390601fdaaf6b30fcc6e5670dd8a55ce311de2f
EvgenS-it <evgens.it101@gmail.com>
2024-08-09 22:38:39 +0100

    Formik logic added

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,37 +1,58 @@
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
+import { Field, Form, Formik } from 'formik';
 
 const ContactForm = ({ onAddContact }) => {
   const { form, label, title, input, btn } = css;
 
-  const handleSubmit = event => {
-    event.preventDefault();
+  // NOT control form data collection without Formik
+  // const handleSubmit = event => {
+  //   event.preventDefault();
 
-    const formElements = event.currentTarget.elements;
-    const name = formElements.contactName.value;
-    const number = formElements.contactNumber.value;
-    const contactObject = { name, number };
+  //   const formElements = event.currentTarget.elements;
+  //   const name = formElements.contactName.value;
+  //   const number = formElements.contactNumber.value;
+  //   const contactObject = { name, number };
+
+  //   onAddContact(contactObject);
+
+  //   event.currentTarget.reset();
+  // };
+
+  const handleSubmit = (values, actions) => {
+    const contactObject = {
+      name: values.contactName,
+      number: values.contactNumber,
+    };
 
     onAddContact(contactObject);
 
-    event.currentTarget.reset();
+    console.log(values);
+    actions.resetForm();
+  };
+
+  const INITIAL_VALUES = {
+    contactName: '',
+    contactNumber: '',
   };
 
   return (
-    <form className={form} onSubmit={handleSubmit}>
-      <label className={label}>
-        <span className={title}>Name</span>
-        <input className={input} type="text" name="contactName" required />
-      </label>
-      <label className={label}>
-        <span className={title}>Number</span>
-        <input className={input} type="tel" name="contactNumber" required />
-      </label>
+    <Formik initialValues={INITIAL_VALUES} onSubmit={handleSubmit}>
+      <Form className={form}>
+        <label className={label}>
+          <span className={title}>Name</span>
+          <Field className={input} type="text" name="contactName" required />
+        </label>
+        <label className={label}>
+          <span className={title}>Number</span>
+          <Field className={input} type="tel" name="contactNumber" required />
+        </label>
 
-      <button type="submit" className={btn}>
-        Add contact
-      </button>
-    </form>
+        <button type="submit" className={btn}>
+          Add contact
+        </button>
+      </Form>
+    </Formik>
   );
 };
 
