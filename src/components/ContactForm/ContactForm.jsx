6d3a8f7e50299fc39dd44198f13e0540339caa71
EvgenS-it <evgens.it101@gmail.com>
2024-08-10 12:40:56 +0100

    form validation finished

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,29 @@
 import css from './ContactForm.module.css';
 import PropTypes from 'prop-types';
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+// for Yup
+const phoneRegExp = /^[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}$/;
+
+const ContactValidationSchema = Yup.object().shape({
+  contactName: Yup.string()
+    .min(3, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  contactNumber: Yup.string()
+    .matches(phoneRegExp, "Valid phone number is 'xxx-xxx-xx-xx'")
+    .required('Required'),
+});
+
+// for Formik
+const INITIAL_VALUES = {
+  contactName: '',
+  contactNumber: '',
+};
 
 const ContactForm = ({ onAddContact }) => {
-  const { form, label, title, input, btn } = css;
+  const { form, label, title, input, btn, errorText } = css;
 
   // NOT control form data collection without Formik
   // const handleSubmit = event => {
@@ -31,27 +51,52 @@ const ContactForm = ({ onAddContact }) => {
     actions.resetForm();
   };
 
-  const INITIAL_VALUES = {
-    contactName: '',
-    contactNumber: '',
-  };
-
   return (
-    <Formik initialValues={INITIAL_VALUES} onSubmit={handleSubmit}>
-      <Form className={form}>
-        <label className={label}>
-          <span className={title}>Name</span>
-          <Field className={input} type="text" name="contactName" required />
-        </label>
-        <label className={label}>
-          <span className={title}>Number</span>
-          <Field className={input} type="tel" name="contactNumber" required />
-        </label>
+    <Formik
+      initialValues={INITIAL_VALUES}
+      onSubmit={handleSubmit}
+      validationSchema={ContactValidationSchema}
+    >
+      {({ values, errors }) => {
+        console.log(values, errors);
 
-        <button type="submit" className={btn}>
-          Add contact
-        </button>
-      </Form>
+        return (
+          <Form className={form}>
+            <label className={label}>
+              <span className={title}>Name</span>
+              <Field
+                className={input}
+                type="text"
+                name="contactName"
+                required
+              />
+              <ErrorMessage
+                name="contactName"
+                component="span"
+                className={errorText}
+              />
+            </label>
+            <label className={label}>
+              <span className={title}>Number</span>
+              <Field
+                className={input}
+                type="tel"
+                name="contactNumber"
+                required
+              />
+              <ErrorMessage
+                name="contactNumber"
+                component="span"
+                className={errorText}
+              />
+            </label>
+
+            <button type="submit" className={btn}>
+              Add contact
+            </button>
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
